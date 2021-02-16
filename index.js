39cf981af8cc2168f2e36bbd78b4c4133f671b0a
amit1997 <amitkuntalbsa@gmail.com>
2021-02-16 23:48:57 +0530

    Procfile added for heroku

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/free/slots',jsonParser,(req, res)=>{
     var end_time = new Date(req.body.date  +" "+ constants.END_HOUR);
     free_time_service.get_free_time_slots(start_time, end_time, req.body.timeZone).then(result=>{
         res.status(200).json(result)
-    }).catch(err=> res.status(500).json({"message":"Something went wrong"});)
+    }).catch(err=> res.status(500).json({"message":"Something went wrong"}))
 })
 
 
