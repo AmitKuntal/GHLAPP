901445aa3b7feb0a344c9d2fa42a19efdb706ebd
amit1997 <amitkuntalbsa@gmail.com>
2021-02-16 23:47:38 +0530

    Db Config added for postgresql

--- db/db.js
+++ db/db.js
@@ -1,11 +1,15 @@
 const pg= require('pg');
 
 const client = new pg.Client({
-  user: 'ghluser',
-  host: "localhost",
-  database: 'ghlapp',
-  password: "ghlpass",
-  port: 5432
+  user: 'ztlbqzhtnwgpzb',
+  host: "ec2-3-213-85-90.compute-1.amazonaws.com",
+  database: 'dclacpp7k00i21',
+  password: "4e6569fc3613586bc77443d26c70b7fb780b6f7d4375dab5e9b09e653727120f",
+  connectionString:"postgres://ztlbqzhtnwgpzb:4e6569fc3613586bc77443d26c70b7fb780b6f7d4375dab5e9b09e653727120f@ec2-3-213-85-90.compute-1.amazonaws.com:5432/dclacpp7k00i21",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = client
